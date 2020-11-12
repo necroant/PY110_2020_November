cc30bae13c6c110fa6774a1bf9a041012811e3aa
PYTHON <1@1.com>
2020-11-12 18:40:20 +0300

    map3

--- iterators.py
+++ iterators.py
@@ -11,5 +11,23 @@ def map2():
     ]
     print(list(map(int, num_list)))
 
+def map3():
+    my_floats = [
+        4.356345,
+        6.0934,
+        3.245235,
+        9.77545,
+        2.164234234,
+        8.884234235,
+        4.595235346645
+    ]
+
+    print(list(map(round_, my_floats)))
+
+
+def round_(a, n=2):
+    return round(a,n)
+
 if __name__ == '__main__':
-    map2()
+    # map2()
+    map3()
