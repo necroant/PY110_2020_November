4049b6f5d3e5cec6ff77006080e46977b0e667fc
PYTHON <1@1.com>
2020-11-12 18:32:38 +0300

    map2

--- iterators.py
+++ iterators.py
@@ -1,4 +1,15 @@
 from itertools import count
 
-for i in count(1, 1):   # натуральные числа начиная с 1 с шагом 1
-    print(i)
+def map2():
+    num_list = [
+        "12",
+        "14",
+        3.1415926,
+        5,
+        0xFF,
+        0b1010101010
+    ]
+    print(list(map(int, num_list)))
+
+if __name__ == '__main__':
+    map2()
